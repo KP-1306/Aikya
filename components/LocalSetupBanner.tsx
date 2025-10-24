9a5918a935ae7e704eee8a50039fb4f149824768
KP-1306 <kbisht786@gmail.com>
2025-10-24 20:59:04 +0530

    Create LocalSetupBanner.tsx

---
+++ components/LocalSetupBanner.tsx
@@ -0,0 +1,9 @@
+// components/LocalSetupBanner.tsx
+export default function LocalSetupBanner() {
+  return (
+    <div className="rounded-xl border bg-amber-50 text-amber-800 p-3 text-sm">
+      Personalize your feed: set your <strong>State</strong> in{" "}
+      <a href="/account" className="underline">Account</a> to see more stories near you.
+    </div>
+  );
+}
