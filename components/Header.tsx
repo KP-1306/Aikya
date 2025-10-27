c7c54d0765b52ed947bef0dc07eb69eabf087dc9
KP-1306 <kbisht786@gmail.com>
2025-10-27 22:02:01 +0530

    Create Header.tsx

---
+++ components/Header.tsx
@@ -0,0 +1,6 @@
+<nav className="flex items-center gap-6 text-sm">
+  <a href="/submit" className="hover:underline">Submit</a>
+  <a href="/about" className="hover:underline">About</a>
+  <a href="/contact" className="hover:underline">Contact</a>
+  {/* …Sign in / Avatar etc */}
+</nav>
